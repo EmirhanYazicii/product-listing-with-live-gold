6343029a732b144c9bf854e04e52da116c9bd706
EmirhanYazicii <emiryazici5858@gmail.com>
2025-10-04 16:51:15 +0300

    Prepare backend + frontend for Heroku deployment

--- server.js
+++ server.js
@@ -93,6 +93,12 @@ app.get("/products", async (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server çalışıyor: http://localhost:${PORT}`);
 });
